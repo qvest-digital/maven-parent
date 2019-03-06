3ba8e3b87ba6c7c77c34176b6e1f9e680c921fbc
mirabilos <t.glaser@tarent.de>
2019-03-06 22:12:09 +0100

    improve sortplug.sh to sort executions…
    • by <id/> (now mandatory)
    • ordered by Maven model
    
    <configuration/> inside are NOT sorted, they are raw DOM elements
    and possibly multiline but you can |sort them yourself

--- src/main/ancillary/sortplug.sh
+++ src/main/ancillary/sortplug.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env mksh
 # -*- mode: sh -*-
 #-
-# Copyright © 2018
+# Copyright © 2018, 2019
 #	mirabilos <t.glaser@tarent.de>
 #
 # Provided that these terms and disclaimer and all copyright notices
@@ -74,6 +74,8 @@ while IFS= read -pr line; do
 		el[OPRECOMMENT]+=$line ;;
 	(1:*(	)'<!--'*'-->')
 		el[OINCOMMENT]+=$line ;;
+	([23]:*(	)'<!--'*'-->')
+		ex_curR+=$line ;;
 	(0:*(	)'<!--'*)
 		el[OPRECOMMENT]+=$line
 		while IFS= read -pr line; do
@@ -85,6 +87,13 @@ while IFS= read -pr line; do
 		el[OINCOMMENT]+=$line
 		while IFS= read -pr line; do
 			el[OINCOMMENT]+=\ ${line##*([	 ])}
+			[[ $line = *'-->' ]] && break
+		done
+		[[ $line = *'-->' ]] || die unclosed comment ;;
+	([23]:*(	)'<!--'*)
+		ex_curR+=$line
+		while IFS= read -pr line; do
+			ex_curR+=\ ${line##*([	 ])}
 			[[ $line = *'-->' ]] && break
 		done
 		[[ $line = *'-->' ]] || die unclosed comment ;;
@@ -158,14 +167,61 @@ while IFS= read -pr line; do
 		x=${line#'		<executions>'}
 		el[Oexecutions]=${x%'</executions>'} ;;
 	(1:'		<executions>')
+		ex_cum=
+		ex_curR=
+		state=2 ;;
+	(2:'		</executions>')
+		el[Oexecutions]=$(print -r -- "$ex_cum" | sort | \
+		    sed 's/^[^]*//' | tr -d '\n')$ex_curR
+		state=1 ;;
+	(2:'			<execution>')
+		ex_curID=
+		ex_curP=
+		ex_curG=
+		ex_curIn=
+		ex_curC=
+		state=3 ;;
+	(3:'				<id>'*'</id>')
+		x=${line##'				<id>'*([	 ])}
+		ex_curID=${x%%*([	 ])'</id>'} ;;
+	(3:'				<phase>'*'</phase>')
+		x=${line##'				<phase>'*([	 ])}
+		ex_curP=${x%%*([	 ])'</phase>'} ;;
+	(3:'				<goals>'*'</goals>')
+		x=${line#'				<goals>'}
+		ex_curG=${x%'</goals>'} ;;
+	(3:'				<goals>')
 		x=
 		while IFS= read -pr line; do
-			[[ $line = '		</executions>' ]] && break
+			[[ $line = '				</goals>' ]] && break
 			x+=$line
 		done
-		[[ $line = '		</executions>' ]] || \
-		    die unterminated tag executions
-		el[Oexecutions]=$x ;;
+		[[ $line = '				</goals>' ]] || \
+		    die unterminated tag goals
+		ex_curG=$x ;;
+	(3:'				<inherited>'*'</inherited>')
+		x=${line##'				<inherited>'*([	 ])}
+		ex_curIn=${line%%*([	 ])'</inherited>'} ;;
+	(3:'				<configuration'?([	 ]*)'>')
+		# body is DOM, sort it yourself
+		x=$line
+		while IFS= read -pr line; do
+			x+=$line
+			[[ $line = '				</configuration>' ]] && break
+		done
+		[[ $line = '				</configuration>' ]] || \
+		    die unterminated tag configuration
+		ex_curC=$x ;;
+	(3:'			</execution>')
+		[[ -n $ex_cum ]] && ex_cum+=$'\n'
+		[[ -n $ex_curID ]] || die execution has no ID
+		ex_cum+="${ex_curID}$ex_curR<execution><id>$ex_curID</id>"
+		[[ -n $ex_curP ]] && ex_cum+="<phase>$ex_curP</phase>"
+		[[ -n $ex_curG ]] && ex_cum+="<goals>$ex_curG</goals>"
+		[[ -n $ex_curIn ]] && ex_cum+="<inherited>$ex_curIn</inherited>"
+		ex_cum+="$ex_curC</execution>"
+		ex_curR=
+		state=2 ;;
 	(1:'		<dependencies>'*'</dependencies>')
 		x=${line#'		<dependencies>'}
 		el[Odependencies]=${x%'</dependencies>'} ;;
