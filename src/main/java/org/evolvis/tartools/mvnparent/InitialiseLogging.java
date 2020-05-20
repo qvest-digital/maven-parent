f243c0055243bb20ed46d76b45b8e94faa038eef
mirabilos <t.glaser@tarent.de>
2020-05-20 17:06:11 +0200

    add org.evolvis.tartools.mvnparent.InitialiseLogging helper class
    document this in src/test/resources/logging.properties and even
    add a Main example
    
    as parent POMs must have packaging pom, not jar, add most of the
    jar lifecycle

---
+++ src/main/java/org/evolvis/tartools/mvnparent/InitialiseLogging.java
@@ -0,0 +1,82 @@
+package org.evolvis.tartools.mvnparent;
+
+/*-
+ * Copyright © 2016, 2018, 2019, 2020 mirabilos (t.glaser@tarent.de)
+ * Licensor: tarent solutions GmbH, Bonn
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ * Utility class with helper functions to load a file from the classpath
+ * and initialise JDK14 “jul” logging ({@link java.util.logging})
+ *
+ * @author mirabilos (t.glaser@tarent.de)
+ */
+public final class InitialiseLogging {
+
+private static final String EINIT = "could not initialise logging subsystem";
+private static final String ENOTFOUND = "configuration file %s not found";
+private static final String LOGCFG = "logging.properties";
+private static boolean LOG14INITED = false;
+static Logger LOGGER;
+
+/**
+ * Prevent instantiation (this is a utility class with only static methods)
+ */
+private InitialiseLogging()
+{
+}
+
+public static InputStream
+getResourceAsStream(final String filename)
+{
+	return Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+}
+
+public static void
+InitialiseJDK14Logging()
+{
+	if (LOG14INITED)
+		return;
+
+	final InputStream config = getResourceAsStream(LOGCFG);
+	IOException exception = null;
+
+	if (config == null) {
+		exception = new IOException(String.format(ENOTFOUND, LOGCFG));
+	} else {
+		try {
+			LogManager.getLogManager().readConfiguration(config);
+			LOG14INITED = true;
+		} catch (IOException e) {
+			exception = e;
+		}
+	}
+
+	LOGGER = Logger.getLogger(InitialiseLogging.class.getName());
+	if (exception != null) {
+		LOGGER.log(Level.SEVERE, EINIT, exception);
+	}
+}
+
+}
---
+++ src/test/java/org/evolvis/tartools/mvnparent/InitialiseLoggingTest.java
@@ -0,0 +1,78 @@
+package org.evolvis.tartools.mvnparent;
+
+/*-
+ * Copyright © 2016, 2018, 2019, 2020 mirabilos (t.glaser@tarent.de)
+ * Licensor: tarent solutions GmbH, Bonn
+ *
+ * Provided that these terms and disclaimer and all copyright notices
+ * are retained or reproduced in an accompanying document, permission
+ * is granted to deal in this work without restriction, including un‐
+ * limited rights to use, publicly perform, distribute, sell, modify,
+ * merge, give away, or sublicence.
+ *
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+ * the utmost extent permitted by applicable law, neither express nor
+ * implied; without malicious intent or gross negligence. In no event
+ * may a licensor, author or contributor be held liable for indirect,
+ * direct, other damage, loss, or other issues arising in any way out
+ * of dealing in the work, even if advised of the possibility of such
+ * damage or existence of a defect, except proven that it results out
+ * of said person’s immediate fault when using the work as intended.
+ */
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+import static net.trajano.commons.testing.UtilityClassTestUtil.assertUtilityClassWellDefined;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InitialiseLoggingTest {
+
+@Test
+public void testUtilityClass() throws ReflectiveOperationException
+{
+	assertUtilityClassWellDefined(InitialiseLogging.class);
+}
+
+private static byte[]
+bytesFrominputStream(final InputStream is) throws IOException
+{
+	try (final ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+		final byte[] buf = new byte[512];
+		int n;
+
+		while ((n = is.read(buf)) != -1)
+			baos.write(buf, 0, n);
+		return baos.toByteArray();
+	}
+}
+
+@Test
+public void testPos() throws IOException
+{
+	assertNull(InitialiseLogging.LOGGER, "LOGGER was not initialised yet");
+	InitialiseLogging.InitialiseJDK14Logging();
+	assertNotNull(InitialiseLogging.LOGGER, "LOGGER could not be initialised");
+	Logger.getLogger(InitialiseLoggingTest.class.getName()).info("meow");
+
+	InputStream ls = InitialiseLogging.getResourceAsStream("simplelogger.properties");
+	byte[] lb = bytesFrominputStream(ls);
+	byte[] tb = Files.readAllBytes(Paths.get("src/test/resources/simplelogger.properties"));
+	assertArrayEquals(tb, lb, "cannot load a properties file");
+
+	ls = InitialiseLogging.getResourceAsStream("META-INF/Messages.properties");
+	String lS = new String(bytesFrominputStream(ls), StandardCharsets.ISO_8859_1);
+	assertTrue(lS.contains("UtilityClassTestUtil"), "cannot load properties from classpath");
+}
+
+}
